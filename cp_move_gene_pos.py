b3149c1854910b84dab5a413cf93b65b19714deb
earnan <hi@arcsona.cn>
2022-02-16 14:14:49 +0800

    1

---
+++ cp_move_gene_pos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+##########################################################
+#
+#       Filename:   cp_move_gene_pos.py
+#         Author:   yuj@genepioneer.cn
+#    Description:   cp_move_gene_pos.py
+#        Version:   1.0
+#           Time:   2022/02/16 11:58:53
+#  Last Modified:   2021-xx-xx 16: 29: 29
+#        Contact:   hi@arcsona.cn
+#        License:   Copyright (C) 2022
+##########################################################
