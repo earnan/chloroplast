66206d8dacaf66ad641bb7bc9741a1060fd6501c
earnan <hi@arcsona.cn>
2022-02-28 14:15:33 +0800

    final

--- R/Chloroplot.R
+++ R/Chloroplot.R
@@ -6,13 +6,16 @@
 # install.packages("circlize")
 
 
-
 rm(list = ls())
-args <- commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
+args = commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
+# 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名
 wddir = format(args[1])
 gbkfile = format(args[2])
+filename <- paste(format(args[3]), "circular", sep = ".") # 类似python字符串的join()函数
 print(wddir)
 print(gbkfile)
+print(filename)
+
 setwd(wddir)
 library(dplyr)
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/color_complement.R")
@@ -26,5 +29,5 @@ source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/plot_genome.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/read_gb_file.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/test_parameters.R")
 tmp <- PlotTab(gbfile = gbkfile, local.file = T)
-PlotPlastidGenome(plot.tables = tmp)
-# Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/test/222 F:/test/111/Hibiscus_sabdariffa.gbk
+PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE) # 去掉密码子偏好性
+# Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/Hibiscus_sabdariffa/ogdraw F:/Hibiscus_sabdariffa/gene_anno/Hibiscus_sabdariffa.gbk Hibiscus_sabdariffa
