561ed4ed1bfdec1d72b0237136811704c0c6739b
earnan <hi@arcsona.cn>
2022-02-28 14:21:05 +0800

    final

--- pipline_new.pl
+++ pipline_new.pl
@@ -4,7 +4,7 @@
 	##画圈图	多线程下圈图容易错，最后再运行下
 	open (CMD,">","$commands_dir/$sample/ogdraw.commands") or die $!;
 	{
-		$cmd = "Rscript /share/nas1/yuj/software/Chloroplot/Chloroplot.R $outdir/$sample/ogdraw $outdir/$sample/gene_anno/$sample.gbk";
+		$cmd = "Rscript /share/nas1/yuj/software/Chloroplot/Chloroplot.R $outdir/$sample/ogdraw $outdir/$sample/gene_anno/$sample.gbk $sample";
 		
 		print CMD $cmd,"\n";
 	}
