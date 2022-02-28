7358e731614b6521383cc569bf6d5571f60eefd4
earnan <hi@arcsona.cn>
2022-02-28 13:22:12 +0800

    可用

--- R/Chloroplot.R
+++ R/Chloroplot.R
@@ -1,12 +1,19 @@
+# Chloroplot需要以下几种包才能正常运行
+# 可通过以下命令安装
 # install.packages("BiocManager")
 # BiocManager::install("genbankr")
 # BiocManager::install("coRdon")
 # install.packages("circlize")
+
+
+
 rm(list = ls())
 args <- commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
-fi <- format(args[1])
-
-setwd("F:/test/222")
+wddir = format(args[1])
+gbkfile = format(args[2])
+print(wddir)
+print(gbkfile)
+setwd(wddir)
 library(dplyr)
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/color_complement.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/converse_ssc.R")
@@ -18,11 +25,6 @@ source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/parse_gb_file.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/plot_genome.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/read_gb_file.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/test_parameters.R")
-
-# file = paste(args[1],"Hibiscus_sabdariffa.gbk", sep = "")
-
-# print(file)
-df <- PlotTab(gbfile = fi, local.file = T)
-PlotPlastidGenome(plot.tables = df)
-
-# Rscript F:/Chloroplot.R "F:/test/222" "F:/test/111/Hibiscus_sabdariffa.gbk"
+tmp <- PlotTab(gbfile = gbkfile, local.file = T)
+PlotPlastidGenome(plot.tables = tmp)
+# Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/test/222 F:/test/111/Hibiscus_sabdariffa.gbk
