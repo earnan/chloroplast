3ad704d43dbb004686a3c38b818cf0d45f1cec4d
earnan <hi@arcsona.cn>
2022-04-19 14:59:07 +0800

    新增 cp_add_gene_seq.py v1.0

---
+++ annotation/cp_add_gene_seq.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+##########################################################
+#
+#       Filename:   cp_add_gene_seq.py
+#         Author:   yujie
+#    Description:   cp_add_gene_seq.py
+#        Version:   1.0
+#           Time:   2022/04/19 13:43:58
+#  Last Modified:   2022/04/19 13:43:58
+#        Contact:   hi@arcsona.cn
+#        License:   Copyright (C) 2022
+#
+##########################################################
+from Bio import SeqIO
+from Bio.Seq import Seq
+#from icecream import ic
+import argparse
+import linecache
+import os
+import re
+import time
+parser = argparse.ArgumentParser(
+    add_help=False, usage='\
+\npython3   cp_add_gene_seq.py\n\
+step1\n\
+step2\n\
+V1.0')
+optional = parser.add_argument_group('可选项')
+required = parser.add_argument_group('必选项')
+optional.add_argument(
+    '-i', '--infasta', metavar='[infasta]', help='infasta', type=str, default='F:/1.fa', required=False)
+optional.add_argument(
+    '-p', '--posstr', metavar='[pos_str]', help='pos_str', type=str, default='1-5:-;10-15:-;17-20:-', required=False)
+optional.add_argument('-c1', '--flag1', help='run step 1?默认是,不运行则-c1',
+                      action='store_false', required=False)
+optional.add_argument('-c2', '--flag2', help='run step 2?默认否,运行则-c2 ',
+                      action='store_true', required=False)
+optional.add_argument('-h', '--help', action='help', help='[帮助信息]')
+args = parser.parse_args()
+
+# TODO 显示终止密码子所在的位置
+
+# TODO 可以取出分段的序列合并后翻译成蛋白质
+
+# TODO
+
+
+def read_file(infasta):  # 读取文件
+    with open(infasta, 'r') as f:
+        seq = ''
+        for line in f:
+            if not line.startswith('>'):
+                seq += line.strip('\n')
+    return seq
+
+
+def format_pos(pos_str):  # 读取输入的位置为位置列表
+    pos_list = []
+    content = pos_str.split(';')
+    for ele in content:
+        if ele.split(':')[-1] == '-':
+            tmp = ele.split(':')[0]+':'+'-1'
+            pos_list.append(tmp)
+        elif ele.split(':')[-1] == '+':
+            tmp = ele.split(':')[0]+':'+'1'
+            pos_list.append(tmp)
+    return pos_list
+
+
+def ir(s):  # 反向互补
+    re = s[::-1]  # 字符串反向
+    c = ""  # 定义字符串c接收互补序列
+    for i in re:
+        if i == 'A':
+            c = c + 'T'
+        elif i == 'G':
+            c = c + 'C'
+        elif i == 'T':
+            c = c + 'A'
+        elif i == 'C':
+            c = c + 'G'
+    return c
+
+
+def merge_sequence(pos_list, seq):  # 合并获取到的序列
+    if int(pos_list[0].split(':')[-1]) == -1:
+        pos_list = pos_list[::-1]
+    cds_seq = ""
+    for ele in pos_list:  # ele 1-10:-1
+        strand = int(ele.split(':')[-1])
+        start = int(ele.split(':')[0].split('-')[0])-1  # -1后变成索引
+        end = int(ele.split(':')[0].split('-')[-1])
+        if strand == (-1):
+            # ic('minus')
+            # 切片,索引从start-1到end-1,也就是对应start到end的序列
+            # a[1:6] 取的是 索引1 索引2 索引3 索引4 索引5,也就是 第2个 一直到第6个数
+            cds_seq += ir(seq[start:end])
+            # ic(cds_seq)
+        elif strand == (1):
+            # ic('plus')
+            cds_seq += seq[start:end]
+            # ic(cds_seq)
+    return cds_seq
+
+
+if __name__ == '__main__':
+    """
+    #################################################################
+    # 格式化成2016-03-20 11: 45: 39形式
+    begin_time = time.time()
+    start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    print('Start Time : {}'.format(start_time))
+    #################################################################
+    """
+    #pos_list = ['1-5:-1', '10-15:-1', '17-20:-1']
+    #seq = 'ATGCG'+'TCCGT'+'ACTTG'+'GCAGC'+'TTCAA'+'AGCTG'+'CTAGC'
+    seq = read_file(args.infasta)
+    pos_list = format_pos(args.posstr)
+    # ic(pos_list)
+    cds_seq = merge_sequence(pos_list, seq)
+    print(cds_seq)
+    """
+    ###############################################################
+    end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    print('End Time : {}'.format(end_time))
+    print('Already Run {}s'.format(time.time()-begin_time))
+    print('Done')
+    ###############################################################
+    """
