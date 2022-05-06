42b48a646f1e991e8ce362086f1394b51d4203c3
earnan <hi@arcsona.cn>
2022-05-06 18:01:34 +0800

    更正 linux下路径

--- R/Chloroplot.R
+++ R/Chloroplot.R
@@ -7,10 +7,10 @@
 
 
 rm(list = ls())
-args = commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
+args <- commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
 # 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名
-wddir = format(args[1])
-gbkfile = format(args[2])
+wddir <- format(args[1])
+gbkfile <- format(args[2])
 filename <- paste(format(args[3]), "circular", sep = ".") # 类似python字符串的join()函数
 print(wddir)
 print(gbkfile)
@@ -18,16 +18,16 @@ print(filename)
 
 setwd(wddir)
 library(dplyr)
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/color_complement.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/converse_ssc.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/detect_ir.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/GC_count.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/gene_color.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/gene_info.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/parse_gb_file.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/plot_genome.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/read_gb_file.R")
-source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/test_parameters.R")
+source("/share/nas1/yuj/software/Chloroplot/R/color_complement.R")
+source("/share/nas1/yuj/software/Chloroplot/R/converse_ssc.R")
+source("/share/nas1/yuj/software/Chloroplot/R/detect_ir.R")
+source("/share/nas1/yuj/software/Chloroplot/R/GC_count.R")
+source("/share/nas1/yuj/software/Chloroplot/R/gene_color.R")
+source("/share/nas1/yuj/software/Chloroplot/R/gene_info.R")
+source("/share/nas1/yuj/software/Chloroplot/R/parse_gb_file.R")
+source("/share/nas1/yuj/software/Chloroplot/R/plot_genome.R")
+source("/share/nas1/yuj/software/Chloroplot/R/read_gb_file.R")
+source("/share/nas1/yuj/software/Chloroplot/R/test_parameters.R")
 tmp <- PlotTab(gbfile = gbkfile, local.file = T)
 PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE) # 去掉密码子偏好性
 # Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/Hibiscus_sabdariffa/ogdraw F:/Hibiscus_sabdariffa/gene_anno/Hibiscus_sabdariffa.gbk Hibiscus_sabdariffa
