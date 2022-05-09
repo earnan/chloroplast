de229fd87b2a34765a058de6b3c287eedaadaf80
earnan <hi@arcsona.cn>
2022-05-09 10:45:18 +0800

    add cp_misa_format.py

---
+++ annotation/etc/misa/cp_misa_format.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+##########################################################
+#
+#       Filename:   cp_misa_format.py
+#         Author:   yujie
+#    Description:   cp_misa_format.py
+#        Version:   1.0
+#           Time:   2022/05/09 10:44:49
+#  Last Modified:   2022/05/09 10:44:49
+#        Contact:   hi@arcsona.cn
+#        License:   Copyright (C) 2022
+#
+##########################################################
