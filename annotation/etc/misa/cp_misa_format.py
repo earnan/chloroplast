0e208d487948a1693a21a09a12194717c6b477d4
earnan <hi@arcsona.cn>
2022-05-09 13:31:44 +0800

    1

--- annotation/etc/misa/cp_misa_format.py
+++ annotation/etc/misa/cp_misa_format.py
@@ -12,3 +12,29 @@
 #        License:   Copyright (C) 2022
 #
 ##########################################################
+from Bio import SeqIO
+from Bio.Seq import Seq
+from icecream import ic
+import argparse
+import linecache
+import os
+import re
+import time
+parser = argparse.ArgumentParser(
+    add_help=False, usage='\
+\npython3   cp_misa_format.py\n\
+step1\n\
+step2\n\
+V1.0')
+optional = parser.add_argument_group('可选项')
+required = parser.add_argument_group('必选项')
+optional.add_argument(
+    '-i', '--infile', metavar='[infile]', help='infile', type=str, default='E:/', required=False)
+optional.add_argument(
+    '-o', '--outfile', metavar='[outfile]', help='outfile', type=str, default='F:/', required=False)
+optional.add_argument('-c1', '--flag1', help='run step 1?默认是,不运行则-c1',
+                      action='store_false', required=False)
+optional.add_argument('-c2', '--flag2', help='run step 2?默认否,运行则-c2 ',
+                      action='store_true', required=False)
+optional.add_argument('-h', '--help', action='help', help='[帮助信息]')
+args = parser.parse_args()
