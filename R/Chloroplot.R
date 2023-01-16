171ca007f74af0ef47abfe56aef6f6525635f122
earnan <hi@arcsona.cn>
2023-01-16 14:39:18 +0800

    ✨ feat(main): 增加类型判断,支持叶绿体与线粒体基因组

--- R/Chloroplot.R
+++ R/Chloroplot.R
@@ -8,13 +8,20 @@
 
 rm(list = ls())
 args <- commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
-# 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名
+# 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名/gbk类型(cp or mt)
 wddir <- format(args[1])
 gbkfile <- format(args[2])
 filename <- paste(format(args[3]), "circular", sep = ".") # 类似python字符串的join()函数
-print(wddir)
-print(gbkfile)
-print(filename)
+type_flag <- format(args[4]) # cp/mt
+print(paste("gbk:", gbkfile))
+if (type_flag == "mt"){
+  print("genome type: Mitochondrion")
+}
+if (type_flag == "cp"){
+  print("genome type: Chloroplot")
+}
+print(paste("outdir:", wddir))
+print(paste("outfile:", filename))
 
 setwd(wddir)
 library(dplyr)
@@ -29,5 +36,11 @@ source("/share/nas1/yuj/software/Chloroplot/R/plot_genome.R")
 source("/share/nas1/yuj/software/Chloroplot/R/read_gb_file.R")
 source("/share/nas1/yuj/software/Chloroplot/R/test_parameters.R")
 tmp <- PlotTab(gbfile = gbkfile, local.file = T)
-PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE) # 去掉密码子偏好性
+if (type_flag == "mt"){
+  PlotMitGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE)
+}
+if (type_flag == "cp"){
+  PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE)
+}
+# 去掉密码子偏好性 cu.bias = FALSE
 # Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/Hibiscus_sabdariffa/ogdraw F:/Hibiscus_sabdariffa/gene_anno/Hibiscus_sabdariffa.gbk Hibiscus_sabdariffa
--- R/Win_Chloroplot.R
+++ R/Win_Chloroplot.R
@@ -8,12 +8,19 @@
 
 rm(list = ls())
 args <- commandArgs(T) # args = commandArgs(trailingOnly = TRUE)
-# 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名
+# 命令行参数依次为 输出文件夹路径/输入gbk文件路径/样本名/gbk类型(cp or mt)
 wddir <- format(args[1])
 gbkfile <- format(args[2])
 filename <- paste(format(args[3]), "circular", sep = ".") # 类似python字符串的join()函数
-print(paste("outdir:", wddir))
+type_flag <- format(args[4]) # cp/mt
 print(paste("gbk:", gbkfile))
+if (type_flag == "mt"){
+  print("genome type: Mitochondrion")
+}
+if (type_flag == "cp"){
+  print("genome type: Chloroplot")
+}
+print(paste("outdir:", wddir))
 print(paste("outfile:", filename))
 
 setwd(wddir)
@@ -29,6 +36,12 @@ source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/plot_genome.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/read_gb_file.R")
 source("D:/OneDrive - cancer/Bioinfo_analysis/Chloroplot/R/test_parameters.R")
 tmp <- PlotTab(gbfile = gbkfile, local.file = T)
-PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE)
-# 去掉密码子偏好性
-# Rscript E:/OneDrive/jshy信息部/分发项目/script/chloroplast/R/Chloroplot.R F:/Hibiscus_sabdariffa/ogdraw F:/Hibiscus_sabdariffa/gene_anno/Hibiscus_sabdariffa.gbk Hibiscus_sabdariffa
+if (type_flag == "mt"){
+  PlotMitGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE)
+}
+if (type_flag == "cp"){
+  PlotPlastidGenome(plot.tables = tmp, file.name = filename, cu.bias = FALSE)
+}
+# 去掉密码子偏好性 cu.bias = FALSE
+# Rscript E:\OneDrive\jshy信息部\Script\chloroplast\R\Win_Chloroplot.R F:\ F:\Ustilago_esculenta_MT10.gbk Ustilago_esculenta_MT10 mt
+# Rscript E:\OneDrive\jshy信息部\Script\chloroplast\R\Win_Chloroplot.R F:\ E:\Server\3171\complete_dir\04Annotation\Lithocarpus_litseifolius_Hance_Chun\Lithocarpus_litseifolius_Hance_Chun.gbk Lithocarpus_litseifolius_Hance_Chun cp
